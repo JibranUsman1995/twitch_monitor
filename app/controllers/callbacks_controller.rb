7c297c660695c2a779a9dd49a3a6062140099760
Jibran Usman <jibran.95@hotmail.com>
2020-03-18 23:25:42 +0500

    Added token getting procedure

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,22 @@
 class CallbacksController < ApplicationController
   def streamlabs
     @streamlab_log = StreamlabLog.new(message: params.to_s)
+    parms ={
+      client_id: 'mA7ZLjbZFU1gkZ9TX5MOm7p9epDizMVw3FDe9frs',
+      client_secret: 'sTJyXjYZjYBcFUgkqsRFHqFsRe1txSBRlGloH5lQ',
+      scopes: 'donations.create donations.read alerts.create legacy.token socket.token points.read points.write alerts.write credits.write profiles.write jar.write wheel.write mediashare.control',
+      redirect_uri: 'https://twitch-listener-rails.herokuapp.com/callbacks/streamlabs',
+      grant_type: 'authorization_code',
+      code: params[:code]
+    }
+
+    if params[:code]
+      apit_endpoint = 'https://streamlabs.com/api/v1.0/'
+      resp = HTTParty.get("#{apit_endpoint}token?#{parms.to_query}")
+      @streamlab_log.save
+
+      return render json: StreamlabLog.create(message: response.body.to_s), status: :ok
+    end
 
     if @streamlab_log.save
       render json: @streamlab_log, status: :ok
