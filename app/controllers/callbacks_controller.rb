2be5d75e5c52e8de58d27906aaa0ed57ace27475
Jibran Usman <jibran.95@hotmail.com>
2020-03-23 20:12:23 +0500

    Token fixing try 3

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -11,14 +11,17 @@ class CallbacksController < ApplicationController
 
     if params[:code]
       apit_endpoint = 'https://streamlabs.com/api/v1.0/'
-      resp = HTTParty.post("#{apit_endpoint}token?#{parms.to_query}")
+      query_str = parms.map{|k, v| CGI::escape(k.to_s) + '=' + CGI::escape(v)}.join('&')
+      urll = "#{apit_endpoint}token?#{query_str}"
+      resp = HTTParty.post(urll)
       @streamlab_log.save
 
+      StreamlabLog.create(message: urll)
       StreamlabLog.create(message: resp.headers.to_s)
       StreamlabLog.create(message: resp.code.to_s)
       StreamlabLog.create(message: resp.body.to_s)
 
-      return render json: StreamlabLog.last(3), status: :ok
+      return render json: StreamlabLog.last(4), status: :ok
     end
 
     if @streamlab_log.save
