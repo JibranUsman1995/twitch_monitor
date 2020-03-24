60308d5e905bace0e5497c6d16eb45ca28800b40
Jibran Usman <jibran.95@hotmail.com>
2020-03-24 22:58:01 +0500

    Added permit params for active admin

--- app/admin/bits.rb
+++ app/admin/bits.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Bit do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :amount, :message
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :amount, :message]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :name, :amount, :message  
 end
